1a804c7f52cdfaa84101af0db625077ecc348417
Giacomo Secchi <giacomosecchi@gmail.com>
2025-12-18 09:31:15 +0100

    Add site logo SVG and update header structure
    - Added a new SVG file for the site logo located at assets/images/site-logo.svg.
    - Created header.html to include the header pattern for the theme.
    - Implemented header-site-logo-base.php to structure the header with site logo and navigation.
    - Updated theme.json to ensure proper font family settings for the theme.

---
+++ patterns/header/header-site-logo-base.php
@@ -0,0 +1,20 @@
+ciao<!-- wp:group {"metadata":{"name":"Header"},"align":"full","style":{"spacing":{"margin":{"top":"0px"},"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"backgroundColor":"base","layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-base-background-color has-background" style="margin-top:0px;padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30)"><!-- wp:group {"align":"wide","layout":{"type":"flex","justifyContent":"left"}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"style":{"spacing":{"blockGap":"10px"},"layout":{"selfStretch":"fixed","flexSize":"200px"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group">
+    <!-- wp:site-logo {"width":100,"shouldSyncIcon":true} /-->
+     		<!-- wp:image {"align":"center","width":40,"height":40,"sizeSlug":"full","linkDestination":"custom"} -->
+		<figure class="wp-block-image aligncenter size-full is-resized"><a href="/"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ) . '/assets/images/site-logo.svg'; ?>" alt="Farmacia Della Bona logo" width="120" height="61"/></a></figure>
+		<!-- /wp:image -->
+    </div>
+<!-- /wp:group -->
+
+<!-- wp:terms-query {"termQuery":{"perPage":10,"taxonomy":"reparto","order":"asc","orderBy":"name","include":[],"hideEmpty":false,"showNested":false,"inherit":false}} -->
+<div class="wp-block-terms-query"><!-- wp:term-template {"style":{"typography":{"textDecoration":"none","textTransform":"uppercase","fontStyle":"normal","fontWeight":"600"}},"fontSize":"x-small","layout":{"type":"grid","columnCount":3,"minimumColumnWidth":null}} -->
+<!-- wp:term-name {"textAlign":"center","isLink":true,"style":{"typography":{"textDecoration":"none"}}} /-->
+<!-- /wp:term-template --></div>
+<!-- /wp:terms-query -->
+
+<!-- wp:navigation {"ref":7,"textColor":"primary","style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"700"}},"fontSize":"x-small","layout":{"type":"flex","setCascadingProperties":true}} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
