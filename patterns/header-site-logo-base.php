c1bfbc4851174dffc89c65a8429bae8033e7d832
Giacomo Secchi <giacomosecchi@gmail.com>
2025-12-18 14:06:03 +0100

    Add header site logo pattern with custom markup

--- patterns/header-site-logo-base.php
+++ patterns/header-site-logo-base.php
@@ -1,10 +1,17 @@
-ciao<!-- wp:group {"metadata":{"name":"Header"},"align":"full","style":{"spacing":{"margin":{"top":"0px"},"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"backgroundColor":"base","layout":{"inherit":true,"type":"constrained"}} -->
+<?php
+/**
+ * Title: Header - custom site logo
+ * Slug: farmacia-della-bona/header-site-logo
+ * Categories: header
+ * Block Types: core/template-part/header
+ */
+?>
+<!-- wp:group {"metadata":{"name":"Header"},"align":"full","style":{"spacing":{"margin":{"top":"0px"},"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"backgroundColor":"base","layout":{"inherit":true,"type":"constrained"}} -->
 <div class="wp-block-group alignfull has-base-background-color has-background" style="margin-top:0px;padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30)"><!-- wp:group {"align":"wide","layout":{"type":"flex","justifyContent":"left"}} -->
 <div class="wp-block-group alignwide"><!-- wp:group {"style":{"spacing":{"blockGap":"10px"},"layout":{"selfStretch":"fixed","flexSize":"200px"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
 <div class="wp-block-group">
-    <!-- wp:site-logo {"width":100,"shouldSyncIcon":true} /-->
-     		<!-- wp:image {"align":"center","width":40,"height":40,"sizeSlug":"full","linkDestination":"custom"} -->
-		<figure class="wp-block-image aligncenter size-full is-resized"><a href="/"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ) . '/assets/images/site-logo.svg'; ?>" alt="Farmacia Della Bona logo" width="120" height="61"/></a></figure>
+		<!-- wp:image {"align":"center","width":140,"height":71,"sizeSlug":"full","linkDestination":"custom"} -->
+		<figure class="wp-block-image aligncenter size-full is-resized"><a href="/"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ) . '/assets/images/site-logo.svg'; ?>" alt="Farmacia Della Bona logo" width="140" height="71"/></a></figure>
 		<!-- /wp:image -->
     </div>
 <!-- /wp:group -->
