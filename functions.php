fe89db5eb8f5ace76ed3beb1a50e1189e5bad5ff
Giacomo Secchi <giacomosecchi@gmail.com>
2025-12-18 14:05:14 +0100

    Add Hero Banner block with ACF fields and template

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+
+
+// Here we call our function on init.
+add_action( 'init', function () {
+    /**
+     * We register our block's with WordPress's handy
+     * register_block_type();
+     *
+     * @link https://developer.wordpress.org/reference/functions/register_block_type/
+     */
+    register_block_type( __DIR__ . '/blocks/hero' );
+});
