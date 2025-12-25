d3738d66ec522d705a1bf22656e13cb8bf642219
Giacomo Secchi <giacomosecchi@gmail.com>
2025-12-25 20:05:46 +0100

    Add ACF group and post type for team members; enhance home and taxonomy templates with staff highlight query

--- functions.php
+++ functions.php
@@ -24,3 +24,56 @@ add_action( 'init', function () {
     );
 } );
 
+
+
+ 
+
+add_filter( 'get_block_type_variations', function ( $variations, $block_type ) {
+
+	if ( 'core/query' === $block_type->name ) {
+		$variations[] =  [
+            'name'       => 'staff-highlight-query',
+            'title'      => __( 'Membro Staff in Evidenza', 'farmacia-della-bona' ),
+            'description' => __( 'Mostra il membro selezionato per questa categoria', 'farmacia-della-bona' ),
+            'isActive'   => [ 'namespace' ],
+            'attributes' => [
+                'namespace' => 'staff-highlight-query',
+                'query'     => [
+                    'postType' => 'team_member',
+                    'perPage'  => 1,
+                    'inherit'  => false,
+                ],
+            ],
+            'scope'      => [ 'inserter' ], // Lo rende visibile nel menu dei blocchi
+        ];
+	}
+
+	return $variations;
+}, 10, 2 );
+
+
+
+add_filter( 'query_loop_block_query_vars', function( $query_vars, $block ) {
+    var_dump($block);
+    if ( ! isset( $block->attributes['namespace'] ) || 'staff-highlight-query' !== $block->attributes['namespace'] ) {        return $query_vars;
+        return $query_vars;
+    }
+   
+    if ( ! is_tax('reparto') ) {
+        return $query_vars;
+    }
+
+    $term_id = get_queried_object_id();
+
+
+    // Recupera l'ID dello staff selezionato nel campo ACF
+    $selected_staff_id = get_field( 'department_manager', 'term_' . $term_id );
+    if ( $selected_staff_id ) {
+        $query_vars['post__in'] = [(int)$selected_staff_id];
+        $query_vars['post_type'] = 'team_member';
+        $query_vars['posts_per_page'] = 1;
+
+    }
+ 
+    return $query_vars;
+}, 10, 2 );
