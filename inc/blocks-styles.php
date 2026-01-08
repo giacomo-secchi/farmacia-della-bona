771e0cdced1fe7ce9ffce50e21a52732d7639bd9
Giacomo Secchi <giacomosecchi@gmail.com>
2026-01-08 15:14:03 +0100

    Blocks styles

---
+++ inc/blocks-styles.php
@@ -0,0 +1,24 @@
+<?php
+
+add_action( 'init', function() {
+    register_block_style( 'core/cover', [
+        'name'  => 'background-1',
+        'label' => 'Background 1',
+        'inline_style' => '.is-style-background-1 .wp-block-cover__background { 
+            background-image: url( "' . get_theme_file_uri( 'assets/images/background-1.svg' ) . '" ); 
+            background-size: cover;
+            background-position: center;
+        }'
+    ] );
+
+    register_block_style( 'core/cover', [
+        'name'  => 'background-2',
+        'label' => 'Background 2',
+        'inline_style' => '.is-style-background-2 .wp-block-cover__background { 
+            background-image: url( "' . get_theme_file_uri( 'assets/images/background-2.svg' ) . '" ); 
+            background-size: cover;
+            background-position: center;
+        }'
+    ] );
+} );
+ 
