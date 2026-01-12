f33f662674824ba2d90061fcc861425bc7094601
Giacomo Secchi <giacomosecchi@gmail.com>
2026-01-12 16:34:41 +0100

    Add Reviews banner

---
+++ patterns/reviews.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Title: Expert Section
+ * Slug: farmacia-della-bona/reviews
+ * Categories: farmacia-della-bona-patterns
+ */
+?>
+<!-- wp:group {"className":"is-style-section-1","align":"full","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull is-style-section-1">
+<!-- wp:paragraph -->
+<p>I nostri clienti</p>
+<!-- /wp:paragraph -->
+ <!-- wp:heading -->
+<h2 class="wp-block-heading">Recensioni e Valutazioni</h2>
+<!-- /wp:heading -->
+
+    	<!-- wp:group {"align":"wide","layout":{"type":"flex","justifyContent":"space-between"}} -->
+		<div class="wp-block-group alignwide">
+			<script defer async src='https://cdn.trustindex.io/loader.js?2f0cacb62b790610b616ca1e87b'></script>
+		</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
