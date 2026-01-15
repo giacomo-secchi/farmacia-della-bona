bc1251bbdddf071199af61f76110b1ee00fbef1a
Giacomo Secchi <giacomosecchi@gmail.com>
2026-01-15 16:15:24 +0100

    Add contact form

--- patterns/contact-form.php
+++ patterns/contact-form.php
@@ -6,56 +6,80 @@
  */
 ?>
 
-<!-- wp:cover {"overlayColor":"secondary","isUserOverlayColor":true,"isDark":false,"sizeSlug":"large","align":"full","className":"is-style-background-2","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-cover alignfull is-light is-style-background-2" style="margin-top:0;margin-bottom:0"><span aria-hidden="true" class="wp-block-cover__background has-secondary-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:columns {"verticalAlignment":"center","align":"wide"} -->
-<div class="wp-block-columns alignwide are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center"} -->
-<div class="wp-block-column is-vertically-aligned-center"><!-- wp:heading {"style":{"typography":{"fontSize":"48px"}}} -->
-<h2 class="wp-block-heading" style="font-size:48px">Come Possiamo Aiutarti?</h2>
+<!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ) . '/assets/images/DSC_4840.jpg'; ?>","dimRatio":90,"overlayColor":"primary","isUserOverlayColor":true,"sizeSlug":"large","align":"full","className":"is-dark is-style-background-2","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-cover alignfull is-dark is-style-background-2" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80)"><img class="wp-block-cover__image-background size-large" alt="" src="<?php echo esc_url( get_stylesheet_directory_uri() ) . '/assets/images/DSC_4840.jpg'; ?>" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim-90 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:heading {"fontSize":"xx-large"} -->
+<h2 class="wp-block-heading has-xx-large-font-size">Contattaci</h2>
 <!-- /wp:heading -->
 
-<!-- wp:jetpack/contact-info -->
-<div class="wp-block-jetpack-contact-info"><!-- wp:jetpack/address {"address":"Via Vignolese 1257","addressLine2":"Località San Damaso","city":"Modena","postal":"41121","linkToGoogleMaps":true} -->
+<!-- wp:columns {"verticalAlignment":null,"align":"wide","className":"is-style-default"} -->
+<div class="wp-block-columns alignwide is-style-default"><!-- wp:column {"verticalAlignment":"center","className":"is-style-default","style":{"spacing":{"blockGap":"0"}}} -->
+<div class="wp-block-column is-vertically-aligned-center is-style-default"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
+<div class="wp-block-group"><!-- wp:jetpack/contact-info {"textColor":"base","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"margin":{"top":"0","bottom":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"layout":{"selfStretch":"fill","flexSize":null}}} -->
+<div class="wp-block-jetpack-contact-info has-base-color has-text-color has-link-color" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:heading {"level":4,"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+<h4 class="wp-block-heading" style="margin-top:0;margin-bottom:0">Indirizzo</h4>
+<!-- /wp:heading -->
+
+<!-- wp:jetpack/address {"address":"Via Vignolese 1257","addressLine2":"Località San Damaso","city":"Modena","postal":"41121","linkToGoogleMaps":true} -->
 <div class="wp-block-jetpack-address"><a href="https://www.google.com/maps/search/Via+Vignolese 1257,Località San Damaso,+Modena,+41121" target="_blank" rel="noopener noreferrer" title="Apri l'indirizzo su Google Maps"><div class="jetpack-address__address jetpack-address__address1">Via Vignolese 1257</div><div class="jetpack-address__address jetpack-address__address2">Località San Damaso</div><div><span class="jetpack-address__city">Modena</span>, <span class="jetpack-address__region"></span> <span class="jetpack-address__postal">41121</span></div></a></div>
-<!-- /wp:jetpack/address -->
+<!-- /wp:jetpack/address --></div>
+<!-- /wp:jetpack/contact-info -->
+
+<!-- wp:html -->
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2840.644312470381!2d10.964632687885578!3d44.60429989648663!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x477fe638e9d5dee7%3A0x776dcdf802e4a509!2sFarmacia%20DELLA%20BONA!5e0!3m2!1sen!2sit!4v1768484422016!5m2!1sen!2sit"
+	 width="350px" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+<!-- /wp:html --></div>
+<!-- /wp:group -->
+
+<!-- wp:jetpack/contact-info {"textColor":"base","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"margin":{"top":"0","bottom":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"layout":{"selfStretch":"fill","flexSize":null}}} -->
+<div class="wp-block-jetpack-contact-info has-base-color has-text-color has-link-color" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:heading {"level":4,"style":{"spacing":{"margin":{"bottom":"0"}}}} -->
+<h4 class="wp-block-heading" style="margin-bottom:0">Numero di telefono e Fax</h4>
+<!-- /wp:heading -->
+
+<!-- wp:jetpack/phone {"phone":"059 469108"} -->
+<div class="wp-block-jetpack-phone"><a href="tel:059469108">059 469108</a></div>
+<!-- /wp:jetpack/phone -->
+
+<!-- wp:jetpack/phone {"phone":"059 465354"} -->
+<div class="wp-block-jetpack-phone"><a href="tel:059465354">059 465354</a></div>
+<!-- /wp:jetpack/phone --></div>
+<!-- /wp:jetpack/contact-info -->
+
+<!-- wp:jetpack/contact-info {"textColor":"base","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"margin":{"top":"0","bottom":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}}} -->
+<div class="wp-block-jetpack-contact-info has-base-color has-text-color has-link-color" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:heading {"level":4,"style":{"spacing":{"margin":{"bottom":"0"}}}} -->
+<h4 class="wp-block-heading" style="margin-bottom:0">Email</h4>
+<!-- /wp:heading -->
 
 <!-- wp:jetpack/email {"email":"info@farmaciadellabona.com"} -->
 <div class="wp-block-jetpack-email"><a href="mailto:info@farmaciadellabona.com">info@farmaciadellabona.com</a></div>
 <!-- /wp:jetpack/email -->
 
- 
-</div>
+<!-- wp:jetpack/email {"email":"ordini@farmaciadellabona.com"} -->
+<div class="wp-block-jetpack-email"><a href="mailto:ordini@farmaciadellabona.com">ordini@farmaciadellabona.com</a></div>
+<!-- /wp:jetpack/email --></div>
 <!-- /wp:jetpack/contact-info -->
 
+<!-- wp:heading {"level":4,"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|20","top":"var:preset|spacing|40"}}}} -->
+<h4 class="wp-block-heading" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--20)">Orari</h4>
+<!-- /wp:heading -->
 
+<!-- wp:table {"hasFixedLayout":false,"style":{"border":{"style":"dotted","width":"1px"}},"borderColor":"neutral"} -->
+<figure class="wp-block-table"><table class="has-border-color has-neutral-border-color" style="border-style:dotted;border-width:1px"><tbody><tr><td></td><td>Mattina</td><td>Pomeriggio</td></tr><tr><td>Da lunedì a venerdì</td><td>8:15 - 12:45</td><td>15:30 - 19:30</td></tr><tr><td>Sabato</td><td>8:15 - 12:45</td><td>Chiuso</td></tr><tr><td>Domenica</td><td>Chiuso</td><td>Chiuso</td></tr></tbody></table></figure>
+<!-- /wp:table -->
 
+<!-- wp:heading {"level":4,"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|20"}}}} -->
+<h4 class="wp-block-heading" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--20)">Social</h4>
+<!-- /wp:heading -->
 
-<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} --><div class="wp-block-group">
-<!-- wp:image {"width":"16px","height":"16px","scale":"cover","sizeSlug":"large"} -->
-<figure class="wp-block-image size-large is-resized"><img src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-printer' viewBox='0 0 16 16'%3E%3Cpath d='M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1'/%3E%3Cpath d='M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1'/%3E%3C/svg%3E" alt="" style="object-fit:cover;width:16px;height:16px"/></figure>
-<!-- /wp:image -->
+<!-- wp:social-links {"iconColor":"base","iconColorValue":"#ffffff","className":"is-style-logos-only"} -->
+<ul class="wp-block-social-links has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"https://www.facebook.com/farmaciadellabona/","service":"facebook"} /-->
 
-<!-- wp:jetpack/contact-info -->
-<div class="wp-block-jetpack-contact-info">
-	<!-- wp:jetpack/phone {"phone":"34333"} -->
-	<div class="wp-block-jetpack-phone"><a href="tel:34333">34333</a></div>
-	<!-- /wp:jetpack/phone -->
-</div>
-<!-- /wp:jetpack/contact-info --></div>
-<!-- /wp:group -->
-
-
-
-
-
-
-<!-- wp:jetpack/map -->
-<div class="wp-block-jetpack-map" data-map-style="default" data-map-details="true" data-points="[]" data-zoom="13" data-map-center="{&quot;longitude&quot;:-122.41941550000001,&quot;latitude&quot;:37.7749295}" data-marker-color="red" data-show-fullscreen-button="true"></div>
-<!-- /wp:jetpack/map --></div>
+<!-- wp:social-link {"url":"https://www.instagram.com/farmaciadellabona/","service":"instagram"} /--></ul>
+<!-- /wp:social-links --></div>
 <!-- /wp:column -->
 
-<!-- wp:column {"verticalAlignment":"center","className":"is-style-card"} -->
-<div class="wp-block-column is-vertically-aligned-center is-style-card"><!-- wp:heading -->
-<h2 class="wp-block-heading">Come possiamo aiutarti?</h2>
+<!-- wp:column {"verticalAlignment":"top","width":"","className":"is-style-card","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40","right":"var:preset|spacing|40"}}}} -->
+<div class="wp-block-column is-vertically-aligned-top is-style-card" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><!-- wp:heading {"className":"is-style-default"} -->
+<h2 class="wp-block-heading is-style-default">Inviaci un messaggio</h2>
 <!-- /wp:heading -->
 
 <!-- wp:jetpack/contact-form {"confirmationType":"text","jetpackCRM":false,"variationName":"default","salesforceData":{"organizationId":"","sendToSalesforce":false},"mailpoet":{"listId":null,"listName":null,"enabledForForm":false}} -->
@@ -71,7 +95,7 @@
 <!-- wp:jetpack/input /--></div>
 <!-- /wp:jetpack/field-email -->
 
-<!-- wp:jetpack/field-telephone {"required":true,"showCountrySelector":true,"default":"US"} -->
+<!-- wp:jetpack/field-telephone {"required":true,"showCountrySelector":true,"default":"IT"} -->
 <div><!-- wp:jetpack/label {"label":"Numero di telefono"} /-->
 
 <!-- wp:jetpack/phone-input /--></div>
